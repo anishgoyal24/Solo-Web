987d4a0838dfd56b846fec90b7c64d05d06be22c
Anish Goyal <anishgoyal2406@gmail.com>
2019-01-10 20:46:29 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+$(window).on('load', function(){
+    $('#status').fadeOut('slow');
+    $('#preloader').fadeOut('slow');
+});
+
+$(function(){
+    $('#team-members').owlCarousel({
+        items:2,
+        autoplay:true,
+        smartSpeed:700,
+        loop:true,
+        autoPlayHoverPause:true,
+        nav:true,
+        dots:false,
+        navText:['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>' ]
+    })
+});
+
+$(function(){
+    $('#progress-elements').waypoint(function(){
+        $(".progress-bar").each(function(){
+            $(this).animate({width: $(this).attr("aria-valuenow") + "%"}, 1000);
+        });
+        this.destroy();
+    }, {offset:'bottom-in-view'});
+});
